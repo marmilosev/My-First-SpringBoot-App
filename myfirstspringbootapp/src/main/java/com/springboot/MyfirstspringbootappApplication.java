86eb68f40919484bb8b53b21ff8ee010cb0b4310
Marija Milosevic <80062620+marmilosev@users.noreply.github.com>
2023-02-17 22:40:01 +0100

    Hello World

---
+++ myfirstspringbootapp/src/main/java/com/springboot/MyfirstspringbootappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //annotation that this is starting point of Application
+public class MyfirstspringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringbootappApplication.class, args);
+	}
+
+}
